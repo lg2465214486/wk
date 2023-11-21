fca55de93c165a899af23e2f7bb3db2fafe4676f
rocky <miataxiang@163.com>
2023-11-21 21:40:02 +0800

    web mvc

--- wk-api/src/main/java/com/example/wk/config/WebMvcConfig.java
+++ wk-api/src/main/java/com/example/wk/config/WebMvcConfig.java
@@ -1,24 +1,67 @@
 package com.example.wk.config;
 
-
+import com.example.wk.entity.WkUser;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.concurrent.TimeUnit;
 
+@Slf4j
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
 
+    private static final String SESSION_KEY = "session:admin:";
+
+    @Autowired
+    private RedisTemplate<String, WkUser> redisTemplate;
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedMethods("*").allowedOrigins("*").allowCredentials(true);
+        registry.addMapping("/**").allowedMethods("*").allowedOriginPatterns("*").allowCredentials(true);
     }
 
-    //@Override
-    //public void addResourceHandlers(ResourceHandlerRegistry registry) {
-    //    registry.addResourceHandler("/**").addResourceLocations("classpath:/static/");
-    //    registry.addResourceHandler("swagger-ui.html").addResourceLocations(
-    //            "classpath:/META-INF/resources/");
-    //}
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new AdminHandlerInterceptor()).addPathPatterns("/**").excludePathPatterns("/admin/**");
+    }
+
+    public class AdminHandlerInterceptor extends HandlerInterceptorAdapter {
+        @Override
+        public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
+            try {
+                if (!request.getMethod().equalsIgnoreCase(RequestMethod.OPTIONS.toString())) {
+                    WkUser user = AdminSession.getInstance().admin();
+                    if (null == user) {
+                        throw new Exception("登录失效");
+                    }else{
+                        String token = request.getHeader("X-Token");
+                        redisTemplate.opsForValue().set(SESSION_KEY + token, user, 30, TimeUnit.MINUTES);
+                    }
+                }
+            }catch (Exception e){
+                log.error("系统错误："+e);
+                PrintWriter out = null;
+                try {
+                    out = response.getWriter();
+                    out.append("{\"code\": 401, \"message\": \"" + e.getMessage() + "\"}");
+                }catch (IOException ex){
+                    log.error(""+ex);
+                    out.close();
+                }
+                return false;
+            }
+            return true;
+        }
+    }
 }
